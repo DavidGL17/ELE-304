ccc37bdbb1920fe746cb655fe27ce5ec7e5ebb96
David Gonzalez Leon <david.gonzalez@citycable.ch>
2021-11-05 08:37:51 +0100

    lab4 finished

--- Bootloader/CM7/Core_CM7/bootloader.h
+++ Bootloader/CM7/Core_CM7/bootloader.h
@@ -1,8 +1,9 @@
 /**
- * @file  {file_name}
- * @brief 
+ * @file  bootloader.h
+ * @brief Defines the main public functions of the bootloader
+
  * @date  28 oct. 2021
- * @author xxpow
+ * @author David González León, Jade Gröli
  *
  */
 #ifndef BOOTLOADER_H_
@@ -10,7 +11,19 @@
 
 #include "main.h"
 
+/**
+ * @brief Initiates the bootloader
+ *
+ * This function will jump to the main app and so never return. All code placed after this function will never execute
+ *
+ */
 void bootloader_init();
+
+/**
+ * @brief printf for the bootloader
+ *
+ * @param fmt The string to print through the uart
+ */
 void printf_bootloader(const char *fmt, ...);
 
 #define PRINTF(...) printf_bootloader(__VA_ARGS__)
